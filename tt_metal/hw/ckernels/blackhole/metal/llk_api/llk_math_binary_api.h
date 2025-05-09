8614681e0c6026611aa4d57b1fa374c779bf68d1
Strahinja Stanisic <sstanisic@tenstorrent.com>
2025-05-09 12:05:55 +0000

    setup eltwise binary for perf

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_math_binary_api.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_math_binary_api.h
@@ -17,10 +17,15 @@ template <
     int NUM_FIDELITY_PHASES = 0,
     EltwiseBinaryReuseDestType binary_reuse_dest = EltwiseBinaryReuseDestType::NONE>
 inline void llk_math_eltwise_binary_init(const std::uint32_t transpose = 0, const std::uint32_t acc_to_dest = 0) {
-    const std::uint32_t num_faces = 4;
+    
+    // math thread will only be clearing valid bits to push unpacker to maximum theoretical throughput
+    ckernel_template tmp(127, 127, TT_OP_CLEARDVALID(0b11, 0));
+    tmp.program(instrn_buffer);
 
-    _llk_math_eltwise_binary_init_<eltwise_binary_type, src_b_bcast_type, NUM_FIDELITY_PHASES, binary_reuse_dest>(
-        num_faces, transpose, acc_to_dest);
+    // const std::uint32_t num_faces = 4;
+
+    // _llk_math_eltwise_binary_init_<eltwise_binary_type, src_b_bcast_type, NUM_FIDELITY_PHASES, binary_reuse_dest>(
+    //     num_faces, transpose, acc_to_dest);
 }
 
 // Version with operands
@@ -74,11 +79,16 @@ inline void llk_math_eltwise_binary(
     const std::uint32_t operand_id = get_operand_id(operand_A);  // both operands must have same number of faces
     const std::uint32_t num_faces = get_operand_num_faces(operand_id);
 
-    _llk_math_eltwise_binary_<
-        eltwise_binary_type,
-        src_b_bcast_type,
-        DST_SYNC_MODE,
-        NUM_FIDELITY_PHASES,
-        binary_reuse_dest,
-        is_fp32_dest_acc_en>(num_faces, dst_index, clear_fp32_dst_acc);
+    // for iterations of mop  should be enough for the unpacker to finish unpacking 512 tiles
+    for(int i=0; i<4; i++) {
+        ckernel_template::run(instrn_buffer);
+    }
+    
+    // _llk_math_eltwise_binary_<
+    //     eltwise_binary_type,
+    //     src_b_bcast_type,
+    //     DST_SYNC_MODE,
+    //     NUM_FIDELITY_PHASES,
+    //     binary_reuse_dest,
+    //     is_fp32_dest_acc_en>(num_faces, dst_index, clear_fp32_dst_acc);
 }
--- tt_metal/kernels/compute/eltwise_binary.cpp
+++ tt_metal/kernels/compute/eltwise_binary.cpp
@@ -8,6 +8,9 @@
 
 #include "compute_kernel_api/eltwise_unary/sfpu_split_includes.h"
 #include "compute_kernel_api/tile_move_copy.h"
+
+#include "tools/profiler/kernel_profiler.hpp"
+
 namespace NAMESPACE {
 void MAIN {
     uint32_t per_core_block_cnt = get_arg_val<uint32_t>(0);
@@ -36,20 +39,23 @@ void MAIN {
     PACK((llk_pack_relu_config(ReluType::ZERO_RELU)));
 #endif
 
-    for (uint32_t block = 0; block < per_core_block_cnt; ++block) {
-        cb_wait_front(cb_inp0, per_core_block_size);
-        cb_wait_front(cb_inp1, per_core_block_size);
-        cb_reserve_back(cb_out0, per_core_block_size);
+    {
+        DeviceZoneScopedN("ELTWISE-BINARY-OP")
 
-        tile_regs_acquire();
+    for (uint32_t block = 0; block < per_core_block_cnt; ++block) {
+            // cb_wait_front(cb_inp0, per_core_block_size);
+            // cb_wait_front(cb_inp1, per_core_block_size);
+            // cb_reserve_back(cb_out0, per_core_block_size);
+
+            // tile_regs_acquire();
 
 #if defined(DST_ACCUM_MODE) || defined(ELTWISE_DEST_REUSE_TYPE)
-        cb_wait_front(cb_in2, per_core_block_size);
-        copy_tile_to_dst_init_short(cb_in2);
-        for (uint32_t i = 0; i < per_core_block_size; ++i) {
-            copy_tile(cb_in2, i, i);  // copy from c_in[0] to DST[0]
+            // cb_wait_front(cb_in2, per_core_block_size);
+            copy_tile_to_dst_init_short(cb_in2);
+            for (uint32_t i = 0; i < per_core_block_size; ++i) {
+                copy_tile(cb_in2, i, i);  // copy from c_in[0] to DST[0]
         }
-        cb_pop_front(cb_in2, per_core_block_size);
+        // cb_pop_front(cb_in2, per_core_block_size);
 #endif
 
 #ifdef DST_ACCUM_MODE
@@ -76,17 +82,21 @@ void MAIN {
             SFPU_OP_CHAIN_0
 #endif
         }
-        tile_regs_commit();
+        // tile_regs_commit();
 
-        tile_regs_wait();
+        // tile_regs_wait();
         for (uint32_t i = 0; i < per_core_block_size; ++i) {
             pack_tile(i, cb_out0);
         }
-        tile_regs_release();
+        // tile_regs_release();
 
-        cb_pop_front(cb_inp0, per_core_block_size);
-        cb_pop_front(cb_inp1, per_core_block_size);
-        cb_push_back(cb_out0, per_core_block_size);
+        // cb_pop_front(cb_inp0, per_core_block_size);
+        // cb_pop_front(cb_inp1, per_core_block_size);
+        // cb_push_back(cb_out0, per_core_block_size);
     }
+        // wait for tensix to finish before exiting zone
+        tensix_sync();
+    }
+
 }
 }  // namespace NAMESPACE
